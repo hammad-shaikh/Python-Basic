843ef039cc11d0f80df516ab73e24bbf46c4a037
Hammad Shaikh <78407095+hammad-shaikh@users.noreply.github.com>
2021-08-22 17:31:22 +0500

    Classic Crossing Game

---
+++ Crossing Game/scoreboard.py
@@ -0,0 +1,25 @@
+FONT = ("Courier", 24, "normal")
+
+from turtle import Turtle
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.level = 1
+        self.hideturtle()
+        self.penup()
+        self.goto(-280,250)
+        # self.write(f"Level: {self.level}", align="left", font=("courier",8,"normal"))
+        self.update_scoreboard()
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Level: {self.level}", align="left", font=("courier",8,"normal"))
+
+
+    def increase_level(self):
+        self.level += 1
+        self.update_scoreboard()
+
+    def game_over(self):
+        self.goto(0,0)
+        self.write(f"Game Over", align="center", font=("courier",8,"normal"))
